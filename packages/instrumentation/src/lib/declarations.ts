80dd5f92fb2491d4f653a54adb5ad8b284d92cae
kszksz <kamknitt@gmail.com>
2022-04-03 23:45:41 +0200

    Refactored declarations and operators

--- packages/instrumentation/src/lib/declarations.ts
+++ packages/instrumentation/src/lib/declarations.ts
@@ -1,49 +1,29 @@
 import { Constructor, ObservableLike } from './types';
 import { getGlobalEnv } from './env';
 
-export function declareConstructor<T extends Constructor<ObservableLike>>(
+const env = getGlobalEnv();
+
+const identityDeclaration: <T>(name: string, target: T) => T = (name, target) =>
+  target;
+
+export const declareConstructor: <T extends Constructor<ObservableLike>>(
   name: string,
   target: T
-): T {
-  const env = getGlobalEnv();
-  if (env) {
-    return env.instrumentConstructor(name, target);
-  } else {
-    return target;
-  }
-}
+) => T = env ? env.instrumentConstructor : identityDeclaration;
 
-export function declareCreator<T extends (...args: any[]) => ObservableLike>(
+export const declareCreator: <T extends (...args: any[]) => ObservableLike>(
   name: string,
   target: T
-): T {
-  const env = getGlobalEnv();
-  if (env) {
-    return env.instrumentCreator(name, target);
-  } else {
-    return target;
-  }
-}
+) => T = env ? env.instrumentCreator : identityDeclaration;
 
-export function declareOperator<
+export const declareOperator: <
   T extends (...args: any[]) => (source: any) => ObservableLike
->(name: string, target: T): T {
-  const env = getGlobalEnv();
-  if (env) {
-    return env.instrumentOperator(name, target);
-  } else {
-    return target;
-  }
-}
-
-export function declareSingleton<T extends ObservableLike>(
+>(
   name: string,
   target: T
-): T {
-  const env = getGlobalEnv();
-  if (env) {
-    return env.instrumentSingleton(name, target);
-  } else {
-    return target;
-  }
-}
+) => T = env ? env.instrumentOperator : identityDeclaration;
+
+export const declareSingleton: <T extends ObservableLike>(
+  name: string,
+  target: T
+) => T = env ? env.instrumentSingleton : identityDeclaration;
--- packages/instrumentation/src/lib/operators.ts
+++ packages/instrumentation/src/lib/operators.ts
@@ -1,15 +1,22 @@
-import { ObservableLike } from './types';
 import { getGlobalEnv } from './env';
 import { Observable } from 'rxjs';
 
-export function tag<T>(tag: string): (source: Observable<T>) => Observable<T> {
-  const env = getGlobalEnv();
-  if (env) {
-    return (source) => {
-      env.addTag(source, tag);
-      return source;
-    };
-  } else {
-    return (source) => source;
-  }
+const env = getGlobalEnv();
+
+export function identityOperator<T>(): (
+  source: Observable<T>
+) => Observable<T> {
+  return (source) => source;
 }
+
+export const tag: <T>(tag: string) => (source: Observable<T>) => Observable<T> =
+  env
+    ? function tagOperator<T>(
+        tag: string
+      ): (source: Observable<T>) => Observable<T> {
+        return (source) => {
+          env.addTag(source, tag);
+          return source;
+        };
+      }
+    : identityOperator;
