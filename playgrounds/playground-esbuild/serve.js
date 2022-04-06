a2da1747da58049bd8daf438cd4c2d579cacd793
kszksz <kamknitt@gmail.com>
2022-04-06 21:53:37 +0200

    Serve demo app

---
+++ playgrounds/playground-esbuild/serve.js
@@ -0,0 +1,15 @@
+const esbuild = require('esbuild');
+const { rxjsInsightsPlugin } = require('@rxjs-insights/plugin-esbuild');
+
+esbuild.serve(
+  { servedir: '.', host: 'localhost', port: 3000 },
+  {
+    entryPoints: ['src/index.ts', 'src/polyfills.ts'],
+    outdir: 'dist',
+    bundle: true,
+    sourcemap: 'linked',
+    keepNames: true,
+    tsconfig: 'tsconfig.json',
+    plugins: [rxjsInsightsPlugin()],
+  }
+);
