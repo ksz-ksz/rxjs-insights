527d5129b148f42c40d456177034a29d294fb67e
kszksz <kamknitt@gmail.com>
2022-05-26 12:59:04 +0200

    color-scheme: dark

--- packages/devtools-extension/src/app/main.tsx
+++ packages/devtools-extension/src/app/main.tsx
@@ -3,4 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app';
 
+if (chrome.devtools.panels.themeName === 'dark') {
+  document.body.classList.add('theme-dark');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
