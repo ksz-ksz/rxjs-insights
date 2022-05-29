ba9c453aadabdfbe25bc706728ef2ff9f22794d9
kszksz <kamknitt@gmail.com>
2022-05-29 06:41:31 +0200

    prevent refetching props/ref

--- packages/devtools-extension/src/app/store/refs/reaction.ts
+++ packages/devtools-extension/src/app/store/refs/reaction.ts
@@ -1,43 +1,60 @@
-import { combineReactions, createReaction, filterActions } from '@lib/store';
+import {
+  combineReactions,
+  createReaction,
+  filterActions,
+  Store,
+} from '@lib/store';
 import { refOutletActions } from '@app/actions/ref-outlet-actions';
-import { filter, from, map, switchMap } from 'rxjs';
+import { EMPTY, filter, from, map, switchMap } from 'rxjs';
 import { refsClient } from '@app/clients/refs';
 import { refsActions } from '@app/actions/refs-actions';
+import { RefsSlice } from '@app/store/refs/slice';
+import { getRefState } from '@app/selectors/refs-selectors';
 
 export const refsReaction = combineReactions()
   .add(
-    createReaction((action$) =>
-      action$.pipe(
-        filterActions(refOutletActions.Expand),
-        switchMap((action) =>
-          from(refsClient.expand(action.payload.refId)).pipe(
-            filter(Boolean),
-            map((props) =>
-              refsActions.PropsLoaded({
-                refId: action.payload.refId,
-                props,
-              })
-            )
-          )
-        )
-      )
+    createReaction(
+      (action$, store) =>
+        action$.pipe(
+          filterActions(refOutletActions.Expand),
+          switchMap((action) => {
+            const { props } = store.get(getRefState(action.payload.refId));
+            return props !== undefined
+              ? EMPTY
+              : from(refsClient.expand(action.payload.refId)).pipe(
+                  filter(Boolean),
+                  map((props) =>
+                    refsActions.PropsLoaded({
+                      refId: action.payload.refId,
+                      props,
+                    })
+                  )
+                );
+          })
+        ),
+      (store: Store<RefsSlice>) => store
     )
   )
   .add(
-    createReaction((action$) =>
-      action$.pipe(
-        filterActions(refOutletActions.InvokeGetter),
-        switchMap((action) =>
-          from(refsClient.invokeGetter(action.payload.refId)).pipe(
-            filter(Boolean),
-            map((ref) =>
-              refsActions.RefLoaded({
-                refId: action.payload.refId,
-                ref,
-              })
-            )
-          )
-        )
-      )
+    createReaction(
+      (action$, store) =>
+        action$.pipe(
+          filterActions(refOutletActions.InvokeGetter),
+          switchMap((action) => {
+            const { ref } = store.get(getRefState(action.payload.refId));
+            return ref !== undefined
+              ? EMPTY
+              : from(refsClient.invokeGetter(action.payload.refId)).pipe(
+                  filter(Boolean),
+                  map((ref) =>
+                    refsActions.RefLoaded({
+                      refId: action.payload.refId,
+                      ref,
+                    })
+                  )
+                );
+          })
+        ),
+      (store: Store<RefsSlice>) => store
     )
   );
