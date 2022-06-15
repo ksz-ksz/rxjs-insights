0e6f862ec4bf8450899c5ef1ccdd0c82a286fb0e
kszksz <kamknitt@gmail.com>
2022-06-15 17:37:26 +0200

    expand indicator

--- packages/devtools-extension/src/app/pages/subscriber-page/subscriber-graph-node-renderer.tsx
+++ packages/devtools-extension/src/app/pages/subscriber-page/subscriber-graph-node-renderer.tsx
@@ -1,5 +1,5 @@
 import { Locations } from '@rxjs-insights/core';
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   DefaultNodeControl,
   NodeControl,
@@ -7,7 +7,7 @@ import {
 } from '@app/components/graph';
 import { RelatedHierarchyNode } from '@app/protocols/insights';
 import { useTheme } from '@mui/material';
-import { useSelector, useSelectorFunction } from '@app/store';
+import { useSelectorFunction } from '@app/store';
 import { timeSelector } from '@app/selectors/insights-selectors';
 import {
   getDirection,
@@ -16,7 +16,10 @@ import {
 } from '@app/pages/subscriber-page/subscriber-graph-utils';
 import gsap from 'gsap';
 import { createSelector, useDispatchCallback } from '@lib/store';
-import { activeSubscriberStateSelector } from '@app/selectors/active-target-state-selector';
+import {
+  activeSubscriberStateSelector,
+  activeSubscriberUiStateSelector,
+} from '@app/selectors/active-target-state-selector';
 import { subscribersGraphActions } from '@app/actions/subscribers-graph-actions';
 
 const circleRadius = 6;
@@ -34,11 +37,16 @@ function getLocationStrings(locations: Locations) {
   }
 }
 
-const vmSelector = (targetId: number) =>
+const vmSelector = (targetId: number, nodeKey: string) =>
   createSelector(
-    [activeSubscriberStateSelector, timeSelector],
-    ([activeSubscriberState, time]) => {
+    [
+      activeSubscriberStateSelector,
+      activeSubscriberUiStateSelector,
+      timeSelector,
+    ],
+    ([activeSubscriberState, activeSubscriberUiState, time]) => {
       const { ref, relations } = activeSubscriberState!;
+      const { expandedKeys } = activeSubscriberUiState!;
       const root = relations.targets[ref.id];
       const target = relations.targets[targetId];
       const event = relations.events[time];
@@ -46,6 +54,11 @@ const vmSelector = (targetId: number) =>
       const isRoot = target.id === ref.id;
       const isActive = target.startTime <= time && time <= target.endTime;
       const isSelected = event && event.target === target.id;
+      const isExpanded = expandedKeys.has(nodeKey);
+      const hasSources =
+        target.sources !== undefined && target.sources.length !== 0;
+      const hasDestinations =
+        target.destinations !== undefined && target.destinations.length !== 0;
 
       return {
         root,
@@ -55,6 +68,9 @@ const vmSelector = (targetId: number) =>
         isRoot,
         isActive,
         isSelected,
+        isExpanded,
+        hasSources,
+        hasDestinations,
       };
     }
   );
@@ -70,7 +86,7 @@ export const SubscriberGraphNodeRenderer = React.forwardRef<
     []
   );
   const theme = useTheme();
-  const vm = useSelectorFunction(vmSelector, node.data.target);
+  const vm = useSelectorFunction(vmSelector, node.data.target, node.data.key);
 
   const targetColors = getTargetColors(theme, vm.target);
   const eventColors = getEventColors(theme, vm.event);
@@ -113,25 +129,17 @@ export const SubscriberGraphNodeRenderer = React.forwardRef<
     }
   }, [vm.isSelected && vm.event.eventType]);
 
+  const primaryColor = vm.isActive
+    ? targetColors.primary
+    : theme.palette.action.disabledBackground;
+  const secondaryColor = vm.isActive
+    ? targetColors.secondary
+    : theme.palette.action.disabled;
+
   return (
     <g ref={elementRef} onClick={toggle}>
-      <circle
-        r="4"
-        fill={
-          vm.isActive ? targetColors.secondary : theme.palette.action.disabled
-        }
-      />
-      {vm.isRoot && (
-        <circle
-          r={5}
-          fill="transparent"
-          stroke={
-            vm.isActive
-              ? targetColors.primary
-              : theme.palette.action.disabledBackground
-          }
-        />
-      )}
+      <circle r="4" fill={secondaryColor} />
+      {vm.isRoot && <circle r={5} fill="transparent" stroke={primaryColor} />}
       {vm.isSelected && (
         <circle
           ref={circleRef}
@@ -139,6 +147,12 @@ export const SubscriberGraphNodeRenderer = React.forwardRef<
           fill="transparent"
           stroke={eventColors.secondary}
         />
+      )}
+      {!vm.isExpanded && vm.hasSources && (
+        <circle cx={-8} r={1} fill={secondaryColor} />
+      )}
+      {!vm.isExpanded && vm.hasDestinations && (
+        <circle cx={8} r={1} fill={secondaryColor} />
       )}
       <text
         fontFamily="Monospace"
