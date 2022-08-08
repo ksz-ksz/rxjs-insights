11db98653f45cafbe8a0f1b28fc13484934bc8b2
kszksz <kamknitt@gmail.com>
2022-08-08 23:13:32 +0200

    prioritize notifications over subscriptions when determining target relationships (risky!)

--- packages/recorder-utils/src/lib/target-utils.ts
+++ packages/recorder-utils/src/lib/target-utils.ts
@@ -1,4 +1,4 @@
-import { Target } from '@rxjs-insights/recorder';
+import { Event, Target } from '@rxjs-insights/recorder';
 import {
   getDestinationNotifications,
   getSourceNotifications,
@@ -18,20 +18,26 @@ export function getSourceTargets(target: Target): Target[] {
 
 export function getRelatedDestinationTargets(target: Target): Target[] {
   const relatedEvents = target.events.flatMap(getDestinationNotifications);
-  const relatedTargets = new Set([
-    ...relatedEvents.map(({ target }) => target),
-    ...getDestinationTargets(target),
-  ]);
+  const relatedTargets = new Set<Target>(
+    relatedEvents.map(({ target }) => target)
+  );
   relatedTargets.delete(target);
+  if (relatedTargets.size === 0) {
+    getDestinationTargets(target).forEach(relatedTargets.add, relatedTargets);
+    relatedTargets.delete(target);
+  }
   return Array.from(relatedTargets);
 }
 
 export function getRelatedSourceTargets(target: Target): Target[] {
   const relatedEvents = target.events.flatMap(getSourceNotifications);
-  const relatedTargets = new Set([
-    ...relatedEvents.map(({ target }) => target),
-    ...getSourceTargets(target),
-  ]);
+  const relatedTargets = new Set<Target>(
+    relatedEvents.map(({ target }) => target)
+  );
   relatedTargets.delete(target);
+  if (relatedTargets.size === 0) {
+    getSourceTargets(target).forEach(relatedTargets.add, relatedTargets);
+    relatedTargets.delete(target);
+  }
   return Array.from(relatedTargets);
 }
