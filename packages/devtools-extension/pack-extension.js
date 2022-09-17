b26eba045d3942d039b03f725fc1867c2c7222f6
kszksz <kamknitt@gmail.com>
2022-09-17 22:36:17 +0200

    Added Firefox scripts

--- packages/devtools-extension/pack-extension.js
+++ packages/devtools-extension/pack-extension.js
@@ -1,36 +1,50 @@
 const fs = require('fs');
 const archiver = require('archiver');
 
-const output = fs.createWriteStream(__dirname + '/extension.zip');
-const archive = archiver('zip', {
-  zlib: { level: 9 },
-});
+const target = process.argv[2];
+switch (target) {
+  case 'firefox':
+    packExtension('firefox');
+    break;
+  case 'chromium':
+    packExtension('chromium');
+    break;
+  default:
+    throw new Error(`Invalid target: ${JSON.stringify(target)}`);
+}
 
-output.on('close', function () {
-  console.log(archive.pointer() + ' total bytes');
-  console.log('extension.zip created');
-});
+function packExtension(target) {
+  const output = fs.createWriteStream(`${__dirname}/extension-${target}.zip`);
+  const archive = archiver('zip', {
+    zlib: { level: 9 },
+  });
 
-archive.on('warning', function (err) {
-  if (err.code === 'ENOENT') {
-  } else {
+  output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('extension.zip created');
+  });
+
+  archive.on('warning', function (err) {
+    if (err.code === 'ENOENT') {
+    } else {
+      throw err;
+    }
+  });
+
+  archive.on('error', function (err) {
     throw err;
-  }
-});
+  });
 
-archive.on('error', function (err) {
-  throw err;
-});
+  archive.pipe(output);
 
-archive.pipe(output);
+  archive.directory('dist/');
+  archive.directory('icons/');
+  archive.file('devtools-page.html');
+  archive.file('index.html');
+  archive.file(`manifest-${target}.json`, { name: 'manifest.json' });
+  archive.file('sources-page.css');
+  archive.file('sources-page.html');
+  archive.file('style.css');
 
-archive.directory('dist/');
-archive.directory('icons/');
-archive.file('devtools-page.html');
-archive.file('index.html');
-archive.file('manifest.json');
-archive.file('sources-page.css');
-archive.file('sources-page.html');
-archive.file('style.css');
-
-archive.finalize();
+  archive.finalize();
+}
--- packages/devtools-extension/serve.js
+++ packages/devtools-extension/serve.js
@@ -1,34 +1,52 @@
 const esbuild = require('esbuild');
 const { rxjsInsightsPlugin } = require('@rxjs-insights/plugin-esbuild');
-const webext = require('web-ext');
 const path = require('path');
+const fs = require('fs');
 
-esbuild.build({
-  entryPoints: [
-    './src/app/main.tsx',
-    './src/devtools-page.ts',
-    './src/sources-page.ts',
-    './src/content-script.ts',
-    './src/page-script.js',
-  ],
-  outdir: 'dist',
-  bundle: true,
-  loader: { '.svg': 'dataurl' },
-  watch: true,
-  sourcemap: 'linked',
-  tsconfig: 'tsconfig.json',
-  plugins: [
-    rxjsInsightsPlugin({
-      installModule: path.join(__dirname, './src/app/install-module.ts'),
-    }),
-  ],
-});
+const target = process.argv[2];
+switch (target) {
+  case 'firefox':
+    serve('firefox');
+    break;
+  case 'chromium':
+    serve('chromium');
+    break;
+  default:
+    throw new Error(`Invalid target: ${JSON.stringify(target)}`);
+}
 
-webext.cmd.run({
-  sourceDir: process.cwd(),
-  target: 'chromium',
-  startUrl: 'http://localhost:3000',
-  chromiumProfile: 'chromium-profile',
-  keepProfileChanges: true,
-  profileCreateIfMissing: true,
-});
+async function serve(target) {
+  esbuild.build({
+    entryPoints: [
+      './src/app/main.tsx',
+      './src/devtools-page.ts',
+      './src/sources-page.ts',
+      './src/content-script.ts',
+      './src/page-script.js',
+    ],
+    outdir: 'dist',
+    bundle: true,
+    loader: { '.svg': 'dataurl' },
+    watch: true,
+    sourcemap: 'linked',
+    tsconfig: 'tsconfig.json',
+    plugins: [
+      rxjsInsightsPlugin({
+        installModule: path.join(__dirname, './src/app/install-module.ts'),
+      }),
+    ],
+  });
+
+  fs.copyFileSync(`manifest-${target}.json`, 'manifest.json');
+
+  (await import('web-ext')).cmd.run({
+    verbose: true,
+    sourceDir: process.cwd(),
+    target: target === 'firefox' ? 'firefox-desktop' : 'chromium',
+    startUrl: 'http://localhost:3000',
+    chromiumProfile: 'chromium-profile',
+    // firefoxProfile: 'firefox-profile', // extension does not always load with that
+    keepProfileChanges: true,
+    profileCreateIfMissing: true,
+  });
+}
