a9e30dfdc04234ac6e0cba6df1aebabcf7766c61
Kamil Knitter <kamil.knitter@dynatrace.com>
2023-08-18 14:07:37 +0200

    actions type fix

--- packages/devtools-extension/src/lib/state-fx/store/actions.ts
+++ packages/devtools-extension/src/lib/state-fx/store/actions.ts
@@ -11,30 +11,39 @@ export interface Action<TPayload> {
   payload: TPayload;
 }
 
-export interface ActionPayloadSpecs {
-  [type: string]: unknown;
-}
-
 export interface CreateActionsOptions {
   namespace: string;
 }
 
-export type Actions<TActionPayloads extends ActionPayloadSpecs> = {
-  [TActionName in keyof TActionPayloads]: ActionFactory<TActionPayloads[TActionName]>;
+export type Actions<TActionPayloads> = {
+  [TActionName in keyof TActionPayloads]: ActionFactory<
+    TActionPayloads[TActionName]
+  >;
 };
 
-export function createActions<TActionPayloads extends ActionPayloadSpecs>({ namespace }: CreateActionsOptions): Actions<TActionPayloads> {
+export function createActions<TActionPayloads>({
+  namespace,
+}: CreateActionsOptions): Actions<TActionPayloads> {
   return new Proxy(
     {},
     {
-      get(target: Record<string, ActionFactory<unknown>>, name: string): ActionFactory<unknown> {
+      get(
+        target: Record<string, ActionFactory<unknown>>,
+        name: string
+      ): ActionFactory<unknown> {
         let actionFactory = target[name];
         if (actionFactory === undefined) {
-          actionFactory = Object.defineProperties((payload: unknown) => ({ namespace, name, payload }), {
-            namespace: { value: namespace },
-            name: { value: name },
-            is: { value: (action: Action<unknown>): action is Action<unknown> => action.namespace === namespace && action.name === name },
-          }) as ActionFactory<unknown>;
+          actionFactory = Object.defineProperties(
+            (payload: unknown) => ({ namespace, name, payload }),
+            {
+              namespace: { value: namespace },
+              name: { value: name },
+              is: {
+                value: (action: Action<unknown>): action is Action<unknown> =>
+                  action.namespace === namespace && action.name === name,
+              },
+            }
+          ) as ActionFactory<unknown>;
           target[name] = actionFactory;
         }
         return actionFactory;
